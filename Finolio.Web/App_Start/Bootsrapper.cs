2e4785f8548d16844a64f6662abe970bf9103d80
Gift C V Chevalsan <giftcv@gmail.com>
2017-03-17 11:50:52 +0530

    Added BundleConfig and Bootstrapper classes

---
+++ Finolio.Web/App_Start/Bootsrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Finolio.Web.App_Start
+{
+    public class Bootsrapper
+    {
+        public static void Run()
+        {
+            
+        }
+
+        private static void SetAutofacContainer()
+        {
+            var builder=new 
+        }
+    }
+}
